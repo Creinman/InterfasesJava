b5a6384e9b69ca32e4d91f5b5b2eda2bd4ab568f
Creinman <68871139+Creinman@users.noreply.github.com>
2021-01-10 15:22:19 +0300

    Update Calculator.java

--- calc/src/Calculator.java
+++ calc/src/Calculator.java
@@ -6,7 +6,7 @@ public class Calculator {
     BinaryOperator<Integer> plus = (x, y) -> {return x + y;};
     BinaryOperator<Integer> minus = (x, y) -> {return x - y;};
     BinaryOperator<Integer> multiply = (x, y) -> {return x * y;};
-    BinaryOperator<Double> devide = (x, y) -> {return x / y;};
+    BinaryOperator<Double> devide = (x, y) -> {return x / y;};   //Double, если оставить Integer - будет считаться ошибкой, тк при делении редко получается целое число
     UnaryOperator<Integer> pow = x -> {return x * x;};
     UnaryOperator<Integer> abs = x -> {if (x>0) return x 
         else if (x<0) return -1*x
